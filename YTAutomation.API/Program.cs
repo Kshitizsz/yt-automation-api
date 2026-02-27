a834737ba5b20dee4771f1343f81d6e98d9c87f5
Kshitiz <insaner17032001@gmail.com>
2026-02-27 12:32:03 +0530

    Text message to confirm backend running fine

--- YTAutomation.API/Program.cs
+++ YTAutomation.API/Program.cs
@@ -166,6 +166,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGet("/", () => "YTAutomation API is running 🚀");
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Run($"http://0.0.0.0:{port}");
